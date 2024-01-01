e5b8ece5fd2c75c1e5b37728b0c4a388c3858570
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-01-01 11:25:21 +0700

    Menambah rute CRUD API versi 1 untuk entitas students menggunakan StudentController

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\V1\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('students', StudentController::class);
 });
